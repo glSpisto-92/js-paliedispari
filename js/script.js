bb7f85fd137a94c01759efea793fe4a3f759ede4
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-03-08 15:20:54 +0100

    Inizio snack

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Crea una funzione per capire se la parola è
+//  palindroma.
+
+var wordPalindrom = prompt('Inserisci parola e scopri se è palindroma!');
+
+for( var i = 0; i < wordPalindrom.length; i++ ){
+
+  console.log(wordPalindrom[i]);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var parola = prompt("Inserire una parola: "), prova="";
+// for (l=parola.length-1; l>=0; l--)
+// {
+// document.write(parola.charAt(l)); //Stampo le lettere invertite al fine di vedere se è giusto il procedimento
+// prova+=parola.charAt(l);
+// }
+// if(parola == prova)
+// document.write(" La parola e' palindroma </br>");
+// else
+// document.write(" La parola non e' palindroma </br>");
