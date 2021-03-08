979d5eca550a1ce12b2a9915d26285659fa473e7
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-03-08 16:15:59 +0100

    Snack 1 ok

--- js/script.js
+++ js/script.js
@@ -3,41 +3,43 @@
 
 var wordPalindrom = prompt('Inserisci parola e scopri se è palindroma!');
 
-for( var i = 0; i < wordPalindrom.length; i++ ){
+function palindroma(parolaArg){
 
-  console.log(wordPalindrom[i]);
+  var wordReverse = "";
+
+  for( var i = parolaArg.length - 1; i >= 0; i-- ){
+
+    wordReverse = wordReverse + parolaArg[i];
+
+    console.log(wordReverse);
+  }
+
+  if( wordReverse == parolaArg){
+
+    alert('la parola è palindroma')
+
+  } else {
+
+    alert('la parola non è palindroma');
+  }
 
 }
+palindroma(wordPalindrom);
 
 
 
 
+// Snack 2.
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
 
+// var pariDispari = prompt('Scegli pari o dispari');
+// var numUtente = parseInt(prompt('Inserisci un numero da 1 a 5'));
+//
+//
+// function random (numero){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var parola = prompt("Inserire una parola: "), prova="";
-// for (l=parola.length-1; l>=0; l--)
-// {
-// document.write(parola.charAt(l)); //Stampo le lettere invertite al fine di vedere se è giusto il procedimento
-// prova+=parola.charAt(l);
-// }
-// if(parola == prova)
-// document.write(" La parola e' palindroma </br>");
-// else
-// document.write(" La parola non e' palindroma </br>");
+  // alert('il numero sorteggiato per la cpu è ' + Math.floor(Math.random() * 5 + 1));
